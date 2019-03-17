507b635319bbeca3fd888cc937b67565eaf02cd2
hannah barker <hannah.m.barker@gmail.com>
2019-03-17 15:42:16 -0400

    second commit

--- main.js
+++ main.js
@@ -4,4 +4,23 @@ $(() => {
     let elementToAddTo = $("#elementToAddTo");
     
     console.log({elementToAdd, elementToAddTo});
+    $("#buttonAppend").on("click", () => {
+        $(elementToAddTo).append(elementToAdd);
+    });
+
+    $("#buttonPrepend").on("click", () => {
+        $(elementToAddTo).prepend(elementToAdd);
+    });
+
+    $("#buttonBefore").on("click", () => {
+        $(elementToAddTo).before(elementToAdd);
+    });
+    $("#buttonAfter").on("click", () => {
+        $(elementToAddTo).after(elementToAdd);
+    });
+
+    $("#buttonUpdate").on("click", () => {
+        // $("#elementToUpdate").html("<h1>Hello World</h1>");
+        $("#elementToUpdate").text("Hello World");
+    });
 });
